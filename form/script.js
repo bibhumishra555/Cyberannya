3c278ce5fa3560b4e07ad047088c8bb6ed647f17
BIBHU KUMAR MISHRA <125743139+bibhumishra555@users.noreply.github.com>
2024-07-05 19:48:56 +0530

    Update script.js

--- form/script.js
+++ form/script.js
@@ -1,59 +1,3 @@
-function sendOtp() {
-        const email = document.getElementById('email').value;
-
-        if (email) {
-            fetch('https://example.com/api/send-otp', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ email: email })
-            })
-            .then(response => response.json())
-            .then(data => {
-                if (data.success) {
-                    alert('OTP has been sent to your email.');
-                } else {
-                    alert('Failed to send OTP. Please try again.');
-                }
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                alert('An error occurred. Please try again.');
-            });
-        } else {
-            alert('Please enter a valid email address.');
-        }
-    }
-
-    function calculatePrice() {
-        const college = document.getElementById('college').value;
-        const course = document.getElementById('course').value;
-        let price = 0;
-
-        switch (college) {
-            case 'LND':
-                price += 1000;
-                break;
-            case 'MS':
-                price += 400;
-                break;
-            case 'SNS':
-                price += 1500;
-                break;
-        }
-
-        switch (course) {
-            case 'UG':
-                price += 200;
-                break;
-            case 'PG':
-                price += 300;
-                break;
-        }
-
-        document.getElementById('priceButton').innerText = 'Price: INR ' + price;
-    }
 
 
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -81,7 +25,6 @@ function submitForm(event) {
     const name = document.getElementById('name').value;
     const mobile = document.getElementById('mobile').value;
     const email = document.getElementById('email').value;
-    const otp = document.getElementById('otp').value;
     const college = document.getElementById('college').value;
     const course = document.getElementById('course').value;
 
@@ -90,7 +33,6 @@ function submitForm(event) {
         name: name,
         mobile: mobile,
         email: email,
-        otp: otp,
         college: college,
         course: course
     })
